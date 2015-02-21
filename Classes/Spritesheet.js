3d72ebf034fd3ae6ab450c4fb35ecba31adffcc3
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2015-02-21 09:43:32 +0100

    Delete test images and migrate prototype into classes

--- Classes/Spritesheet.js
+++ Classes/Spritesheet.js
@@ -5,30 +5,36 @@
 	Date: 19/02/2015
 	
 */
-/*  var ,
-    path   = require('path'),
-    colors = require('colors'),
-   */
 
-var fs     = require('fs'),
-    path   = require('path'),
-    Canvas = require('node-canvas-bin'),
-    binpacking = require('binpacking'),
 
-     u = new(require('./utils'))();
-    u.debug = true;
+var fs          = require('fs'),
+    path        = require('path'),
+    Canvas      = require('node-canvas-bin'),
+    binpacking  = require('binpacking'),
+    Image = Canvas.Image;
+
+var execFile = require('child_process').execFile;
+var pngquant = require('pngquant-bin').path;
+
+
+var u = new(require('./utils'))(); //Call and create a new Utils
+u.debug = false;
 
 var _CONFIGURATION_ = {
 
-    extensions : ['jpg', 'jpeg', 'png'],
-    logs : true,
-    prevent_erase : true
+    extensions           : ['jpg', 'jpeg', 'png'],
+    logs                 : true,
+    prevent_erase        : true,
+    folder_permission    : '0766',
+    sprite_suffix        : '_optimize',
+    sprite_output_dir    : 'SpriteOutput',
+    export_filename      : 'sprite',
+    export_extension     : '.png',
+    optimizing_image     : true
 
 };
 
 
-
-var sprite_dir = '';
 
 var Spritesheet = function Spritesheet(settings){
     u.log('Spritesheet: constructor');
@@ -36,41 +42,72 @@ var Spritesheet = function Spritesheet(settings){
 	//Properties
     //TODO var Packer = binpacking.Packer; WHY ? Read doc !
     var GrowingPacker = binpacking.GrowingPacker;
-
-    this.settings = settings; //TODO NEED A CONTROL
     this.packer = new GrowingPacker;
     this.canvas = new Canvas(0, 0);
     this.ctx = this.canvas.getContext('2d');
-    this.autorized_extensions = ['jpg','png'];
-    this.img_assets = [];
+
+    this.settings = settings; //TODO NEED A CONTROL
+    this.settings_loaded = false;
 
 
-    this.sprite_dir = settings.working_directory+"SpriteGenOutput";
-    this.export_path = sprite_dir+path.sep+'sprite.png';
+    u.log('Spritesheet: config', _CONFIGURATION_);
+};
 
-    u.log('Spritesheet:', _CONFIGURATION_);
+Spritesheet.prototype.processing = function(callback){
+    u.log('Spritesheet: processing');
+
+    var that = this;
+
+    that.verifyingSettings();
+
+    that.grabImageAssetsFromDirectory(that.settings.dir,function(e){
+        u.log('Spritesheet: outFromDirectory', e);
+
+        var dimensions = that.fitCanvasDimensionsWithImageAssets(e.blocks);
+        u.log('Spritesheet: res', dimensions);
+        that.generateSprite(e.blocks, dimensions);
+        that.saveSpriteSheet(function(){
+            u.log('Spritesheet: SAVED');
+
+            callback(e.blocks);
+        });
+
+    });
 };
 
 Spritesheet.prototype.setSettings = function(settings){
     u.log('Spritesheet: setSettings');
     this.settings = settings;
 
-    u.log(settings);
+    u.log('Spritesheet: settings', settings);
+
+    this.verifyingSettings();
+};
+
+Spritesheet.prototype.verifyingSettings = function(){
+    u.log('Spritesheet: verifyingSettings');
+
+    this.settings_loaded = true;
+
 };
 
 Spritesheet.prototype.grabImageAssetsFromDirectory = function (directory,callback){
     u.log('Spritesheet: grabImageAssetsFromDirectory');
+    var settings = this.settings;
 
-    if(settings.working_directory.length > 0 && fs.lstatSync(settings.working_directory).isDirectory()){
-        dir_path = settings.working_directory;
+
+    if(settings.dir.length > 0 && fs.lstatSync(settings.dir).isDirectory()){
+        var dir_path = settings.dir;
+
         var files = fs.readdirSync(dir_path);
         var images = [];
+        var blocks = [];
         for(var i in files) {
             var filename = files[i];
-            var extension = utils.getExtensionName(filename);
+            var extension = u.getExtensionName(filename);
             var full_path = dir_path+path.sep+filename;
 
-            if(settings.auth_extensions.indexOf(getExtension(filename)) != -1){
+            if(_CONFIGURATION_.extensions.indexOf(u.getExtensionName(filename)) != -1){
 
 
 
@@ -84,10 +121,11 @@ Spritesheet.prototype.grabImageAssetsFromDirectory = function (directory,callbac
                     w:img.width ,
                     h:img.height,
                     img:img,
-                    name : formatName(filename)
+                    name : u.formatName(filename)
                 };
 
                 images.push(img);
+
                 blocks.push(block);
 
             }
@@ -95,12 +133,14 @@ Spritesheet.prototype.grabImageAssetsFromDirectory = function (directory,callbac
     }
 
     if(typeof callback != "undefined")
-        callback();
+        callback({'files' : files, 'images' : images, 'blocks' : blocks});
 };
 
 Spritesheet.prototype.fitCanvasDimensionsWithImageAssets = function(img_assets){
     u.log('Spritesheet: fitCanvasDimensionsWithImageAssets');
     this.packer.fit(img_assets);
+
+
 
     var maxWidth = 0;
     var maxHeight = 0;
@@ -122,6 +162,7 @@ Spritesheet.prototype.fitCanvasDimensionsWithImageAssets = function(img_assets){
     }
 
 
+
     return {
         c_width  : maxWidth,
         c_height : maxHeight
@@ -129,7 +170,25 @@ Spritesheet.prototype.fitCanvasDimensionsWithImageAssets = function(img_assets){
 };
 
 
-Spritesheet.prototype.drawAllImageAssets = function(img_assets){
+Spritesheet.prototype.generateSprite = function(img_assets, dimensions){
+
+    var that = this;
+    this.packer.fit(img_assets);
+
+    this.canvas = new Canvas(dimensions.c_width, dimensions.c_height); //TODO MAYBE NOT NEED AND JUST SET WIDTH AND HEIGHT
+    this.ctx = this.canvas.getContext('2d');
+
+
+    that.drawAllImageAssets(img_assets, function(){
+
+        u.log('DRAW END');
+
+    });
+
+};
+
+
+Spritesheet.prototype.drawAllImageAssets = function(img_assets, callback){
     u.log('Spritesheet: drawAllImageAssets');
 
     for(var n = 0 ; n < img_assets.length ; n++) {
@@ -138,22 +197,30 @@ Spritesheet.prototype.drawAllImageAssets = function(img_assets){
             this.ctx.drawImage(img_asset.img, img_asset.fit.x, img_asset.fit.y, img_asset.img.width, img_asset.img.height);
         }
     }
+
+
+    //TODO ADD CALLBACK
+
+    callback();
 };
 
-Spritesheet.prototype.processing = function(){
-    u.log('Spritesheet: setSettings');
-    this.grabImageAssetsFromDirectory(this.settings.directory,function(){
-
-    });
-};
 
 
-Spritesheet.prototype.saveSpriteSheet = function (canvas,name,callback){
+
+Spritesheet.prototype.saveSpriteSheet = function (callback){
     u.log('Spritesheet: saveSpriteSheet');
 
-    var folder_permission = '0766';
-    if(!fs.existsSync(this.sprite_dir)){
-        fs.mkdirSync(this.sprite_dir, folder_permission, function(err){
+    var that = this;
+    var name = this.settings.sprite_name;
+
+    this.output_dir = this.settings.dir + path.sep + _CONFIGURATION_.sprite_output_dir;
+    this.export_path = this.output_dir + path.sep + name + _CONFIGURATION_.export_extension;
+
+
+    u.log(this.output_dir)
+
+    if(!fs.existsSync(this.output_dir)){
+        fs.mkdirSync(this.output_dir, _CONFIGURATION_.folder_permission, function(err){
             if(err){
                 throw(err);
             }
@@ -161,9 +228,11 @@ Spritesheet.prototype.saveSpriteSheet = function (canvas,name,callback){
     }
 
 
+
+
     var bufs = [],
         out = fs.createWriteStream(this.export_path),
-        stream = canvas.createPNGStream();
+        stream = this.canvas.createPNGStream();
 
 
     stream.on('data', function(chunk){
@@ -176,37 +245,44 @@ Spritesheet.prototype.saveSpriteSheet = function (canvas,name,callback){
             return sum + buf.length;
         }, 0);
 
-        var size = utils.formatSize(streamSize);
-        utils.log('[SUCCESS] sprite image saved ['+size+']');
+        var size = u.formatSize(streamSize);
+        u.log('[SUCCESS] sprite image saved ['+size+']');
 
-        if(!utils.isUndefined())
+        if(!_CONFIGURATION_.optimizing_image){
             callback();
+        }else{
+
+            that.optimizingSpritesheet(callback);
+
+        }
+
 
 
     });
+
 };
 
 
-Spritesheet.prototype.optimizingSpritesheet = function(working_directory,filename,callback){
+Spritesheet.prototype.optimizingSpritesheet = function(callback){
     u.log('Spritesheet: optimizingSpritesheet');
 
-    var old_file = dir+path.sep+name+'.png';
-    var optimized_file = dir+path.sep+name+'_opti.png';
+    var old_file = this.export_path;
+    var optimized_file = this.output_dir + path.sep + this.settings.sprite_name + _CONFIGURATION_.sprite_suffix + _CONFIGURATION_.export_extension; //TODO ADD BY DEFAULT IN CONF BUT CAN BE SETTING FILE
 
     if(fs.existsSync(optimized_file))
         fs.unlinkSync(optimized_file); //Remove if exist
 
     execFile(pngquant, ['-o', optimized_file, old_file], function (err) {
+
         if (err) {
             throw err;
         }
 
         var file_stat = fs.statSync(optimized_file);
-        var file_size = utils.formatSize(file_stat.size);
-        log('[SUCCESS] optimized sprite ['+file_size+']');
+        var file_size = u.formatSize(file_stat.size);
+        u.log('[SUCCESS] optimized sprite ['+file_size+']');
 
-        if(!utils.isUndefined())
-            callback();
+        callback();
     });
 
 };
