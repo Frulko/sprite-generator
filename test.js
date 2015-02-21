15f65732efbbb167241224ccf549492ef8ba0e68
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2015-02-21 09:44:14 +0100

    Test script.

--- test.js
+++ test.js
@@ -1,19 +1,35 @@
 var Spritesheet = require('./Classes/Spritesheet');
 var Settings    = require('./Classes/Settings');
 var Stylesheet  = require('./Classes/Stylesheet');
+var u           = new(require('./Classes/utils'))(); //Call and create a new Utils
+u.debug = false;
 
+//TODO GULP OR BRUNCH JSHINT
 
 
 
 var settings = new Settings({
 
-    dir : './test_folder'
+    dir              : './test_folder',
+    sprite_name      : 's_sprite'
+
 
 });
 
 
-var sprite = new Spritesheet(Settings);
+
+
+var sprite = new Spritesheet(settings);
 sprite.setSettings(settings);
+sprite.processing(function(e){
+
+    var stylesheet = new Stylesheet(settings);
+    stylesheet.setSettings(settings);
+    stylesheet.generateCSS(e);
+
+
+
+});
 
 /*sprite.grabImageAssetsFromDirectory('./test_folder', function(){
     console.log('SUCCESS GRABBING')
