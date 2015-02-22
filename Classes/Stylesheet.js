04357ce77eff671ebb03db1485391f54ddfa36e3
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2015-02-22 18:54:29 +0100

    Generate HTML sample file. Add CSS rules. Add Moment JS for date.

--- Classes/Stylesheet.js
+++ Classes/Stylesheet.js
@@ -6,12 +6,19 @@
 
  */
 var fs          = require('fs'),
-    path        = require('path');
+    path        = require('path'),
+    moment      = require('moment'),
+    doT         = require('dot'),
+    html        = require('html');
 
 var u = new(require('./utils'))();
 u.debug = false;
 
 
+var css_rules = [
+ {'_hover' : ':hover'},
+ {'_active' : ':active'}
+];
 
 var Stylesheet = function Stylesheet(settings){
  u.log('Stylesheet: constructor');
@@ -44,14 +51,61 @@ Stylesheet.prototype.verifyingSettings = function(){
 Stylesheet.prototype.generateCSS = function (blocks){
  u.log('Stylesheet: generateCSS');
 
- var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''); //TODO REPLACE WITH MOMENT.JS
+ var date = moment().format('MMMM Do YYYY, h:mm:ss a'); //TODO REPLACE WITH MOMENT.JS
+ var prefix = this.settings.css_prefix;
+ var that = this;
+
+ var name = this.settings.sprite_name;
+ var sprite_filename =  name + '.png';
+
+
  var css = "/* GENERATED CSS - "+date+" */ \n";
- var prefix = "sg-";
- var pos = [{x:651,y:1,name:"sep_109"},{x:12,y:465,name:"btn_b_78"},{x:546,y:18,name:"bordure"},{x:81,y:56,name:"oki"}];
- u.log(blocks);
- for(i in blocks){
+
+ if(that.settings.css_span_block){
+     css += '.icon{ display:block } \n';
+ }
+
+ css += '[class^="' + prefix + '"],[class*="' + prefix + '"] { background:url('+ sprite_filename +') no-repeat top left;} \n';
+
+ var css_items = [];
+
+
+ for(var i in blocks){
+
   var block = blocks[i];
-  css += '.' + prefix+block.name + "{background-position: "+block.fit.x+"px "+block.fit.y+"px;} \n";
+  var classname = block.name;
+
+  var custom_rule = false;
+
+  for(var j in css_rules){
+
+     var rule = css_rules[j];
+
+     var keys = Object.keys(rule);
+     //classname = classname.replace(j, rule);
+
+      u.log(classname.indexOf(keys[0]) + ' --- ' + keys[0].length);
+     if(classname.indexOf(keys[0]) > -1){
+        classname = classname.replace(keys[0], rule[keys]);
+         custom_rule = true;
+
+      break;
+     }
+
+
+
+  }
+
+     if(!custom_rule){
+         css_items.push(that.settings.css_prefix + classname);
+     }
+
+
+
+      css += '.' + prefix + classname + "{ width: " + (block.w - that.settings.padding_x) + "px; height: " + (block.h - that.settings.padding_y) + "px; background-position: -"+block.fit.x+"px -"+block.fit.y+"px; } \n";
+
+
+
  }
 
 
@@ -63,10 +117,40 @@ Stylesheet.prototype.generateCSS = function (blocks){
    u.log(err);
   } else {
    u.log('[SUCCESS] css file saved');
+
+   that.generateHTML(css_items);
+
   }
  });
+
+
+
 };
 
+
+Stylesheet.prototype.generateHTML = function (css_items){
+
+
+    var sprite_dir = this.settings.dir + path.sep + "SpriteOutput";
+    u.loadTemplate('html_generate.dot', function(d){
+        var tempFn = doT.template(d);
+        var resultText = tempFn({items: css_items});
+
+        var prettyData = html.prettyPrint(resultText, {indent_size: 2});
+
+        fs.writeFile(sprite_dir + path.sep + 'sprite.html', prettyData, function(err) {
+            if(err) {
+                u.log(err);
+            } else {
+                u.log('[SUCCESS] html file saved');
+
+            }
+        });
+    });
+
+
+
+};
 
 
 
