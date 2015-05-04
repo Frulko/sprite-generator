152404e274241a7a42ebf0ef8287bde689bf6fc9
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2015-05-04 16:51:55 +0200

    Test file update

--- test.js
+++ test.js
@@ -6,81 +6,30 @@ var Sprite      = require('./Classes/Sprite'),
 
 u.debug = false;
 
-//TODO GULP OR BRUNCH JSHINT
-/*var settings = new Settings({
-
-    dir              : './test_folder',
-    sprite_name      : 's_sprite_new',
-    generate_HTML    : true,
-    css_rules        : true,
-    css_prefix       : 'sg-',
-    css_span_block   : true,
-    padding_x            : 5,
-    padding_y            : 5
 
 
-});
-
-settings.loadJSONFile(function(json){
-    u.log(json);
-});
-
-
-
-var sprite = new Spritesheet(settings);
-sprite.setSettings(settings);
-sprite.processing(function(e){
-
-    var stylesheet = new Stylesheet(settings);
-    stylesheet.setSettings(settings);
-    stylesheet.generateCSS(e);
-    
-});*/
-
-
-var settings = {
-    working_directory : 'test_folder',
-    save_settings     : true,
-    debug             : false,
-    log               : true,
-    backup_previous   : true,
-    output            : {
-        up_directory : false,
-        style    : '',
-        sprite   : ''
-    },
-    style             : {
-        type : 'css',
-        icon : true,
-        prefix : 's-',
-        rules : [
-            {'_hover' : ':hover'},
-            {'_active' : ':active'}
-        ]
-    },
-    sprite            : {
-        name        : 'sprite',
-        compression : true,
-        engine      : 'pngquant', //In the futur
-        arrangement : 'vertical', //horizontal , auto
-        out_directory : 'SpriteGeneration',
-        html_preview : true,
-        padding           : {
-            top     : 0,
-            left    : 0
-        }
+var settings = new Settings({
+    style : {
+        type : 'less'
     }
-};
+});
 
 
-//Sprite.getImages(settings.working_directory);
 var sprite = new Sprite(settings);
-console.log(sprite.getImages(settings.working_directory));
-/*spriteGen(settings, function () {
+var styleSheet = new Stylesheet(settings);
 
-});*/
 
-/*sprite.grabImageAssetsFromDirectory('./test_folder', function(){
-    console.log('SUCCESS GRABBING')
-});*/
+
+sprite.processing(function (res, err){
+    if(err){
+        console.log(err);
+        return [];
+    }
+
+    if(parseInt(res.code[0], 10) === 2){
+        styleSheet.generateCSS(res.sprite.assets);
+    }
+
+});
+
 
