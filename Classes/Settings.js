41b4bd74c87ab4e05c6cd161157c6af59acb172f
Guillaume Dumoulin <guillaume.dumoulin@hotmail.fr>
2015-05-04 16:50:35 +0200

    Add default settings and override object system

--- Classes/Settings.js
+++ Classes/Settings.js
@@ -11,7 +11,42 @@ var fs          = require('fs'),
 
 
 var u = new(require('./utils'))();
-u.debug = false;
+u.debug = true;
+
+
+var default_settings = {
+    working_directory : 'test_folder',
+    save_settings     : true,
+    debug             : false,
+    log               : true,
+    backup_previous   : true,
+    output            : {
+        up_directory : false,
+        stylesheet    : '',
+        sprite   : ''
+    },
+    style             : {
+        type : 'css',
+        icon : true,
+        prefix : 's-',
+        rules : [
+            {'_hover' : ':hover'},
+            {'_active' : ':active'}
+        ]
+    },
+    sprite            : {
+        name          : 'sprite',
+        compression   : true,
+        engine        : 'pngquant', //In the futur
+        arrangement   : 'vertical', //horizontal , auto
+        out_directory : 'SpriteGeneration',
+        html_preview  : true,
+        padding       : {
+            top       : 0,
+            left      : 0
+        }
+    }
+};
 
 
 var Settings = function Settings(settings){
@@ -19,12 +54,47 @@ var Settings = function Settings(settings){
     //Properties
 	this.settings_loaded = false;
 
-
-	u.extend(this, settings);
-
+    //Todo Add 3 control - sprite, style, conf.
+    //Todo Force to specify a working directory
+	//u.extend(default_settings, settings);
+    //console.log(default_settings.style)
+    this.override(settings);
 };
 
+Settings.prototype.override = function (settings) {
 
+    u.log('Settings: override');
+    this.parseObject(default_settings, settings);
+
+    //u.log(Object.keys(default_settings));
+};
+
+Settings.prototype.parseObject = function (object, compareObject){
+    for(var i in object){
+
+        if(compareObject.hasOwnProperty(i)){
+            if(typeof object[i] === 'object'){
+                for(var j in object[i]){
+                    if(compareObject[i].hasOwnProperty(j)){
+                        object[i][j] = compareObject[i][j];
+                    }
+                }
+            }else{
+                object[i] = compareObject[i];
+            }
+
+            if(typeof object[i] === 'object'){
+                this.parseObject(object[i], compareObject);
+            }
+        }
+
+
+    }
+};
+
+Settings.prototype.getSettings = function (){
+    return default_settings;
+};
 
 //Settings.prototype.testSprite = function(){};
 
